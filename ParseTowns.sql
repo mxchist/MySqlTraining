4dc8d67e28d08a2b3436c36790c95e42b97da52e
Max Chistyakov <ilovetolive@inbox.ru>
2020-05-11 23:06:53 +0300

    ParseTowns: xquery[3]

--- ParseTowns.sql
+++ ParseTowns.sql
@@ -5,9 +5,13 @@ from openrowset(bulk 'c:\Users\Max\Desktop\Towns.xml', single_clob) as d (b)
 )
 select 
 	N'Russian Federation'
+	, N', (N'''
 
-	, t.unit.value('(td[position() = 2]/a)[1]', 'nvarchar(1000)')
-	, t.unit.value('(td[position() = 3]/a)[1]', 'nvarchar(1000)')
+	+ replace(	t.unit.value('(td[position() = 2]/a)[1]', 'nvarchar(1000)'), ', Republic of', '') + ''', N'''
+	+ t.unit.value('(td[position() = 3]/a)[1]', 'nvarchar(1000)') + N''')'
 from countries_xml as x
 cross apply x.b.nodes('table/tbody/tr[position() > 1]') as t(unit)
 --where t.unit.exist('th') = 0
+
+
+
