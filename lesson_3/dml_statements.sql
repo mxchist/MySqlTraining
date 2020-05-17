ddbd619801cdb0d29d9af10a1367bc3edc6de486
Max Chistyakov <ilovetolive@inbox.ru>
2020-05-17 18:48:43 +0300

    exercise 2 for employees completed

--- lesson_3/dml_statements.sql
+++ lesson_3/dml_statements.sql
@@ -25,21 +25,44 @@ from geodata._cities as ct
 inner join geodata._regions as r on r.id = ct.region_id
 where r.title = 'Московская область';
 
--- 3 среднюю зарплату по отделам 
+-- 1 среднюю зарплату по отделам 
 
-with average_salaries as (
 select
-*
+	average_salaries.salary_avg
+    , d.dept_name
+from  (
+select
+	avg(sl.salary) as salary_avg
+    , sum(sl.salary) as salary_sum
+    , count(sl.emp_no) as employer_qry
+	, de.dept_no
 from lesson3_db.salaries as sl
 inner join lesson3_db.employees as emp on emp.emp_no = sl.emp_no
 inner join lesson3_db.dept_emp as de on de.emp_no = emp.emp_no
-inner join lesson3_db.departments as d on d.dept_no = de.dept_no
+-- проверка, что по каждому сотруднику учитывалась только зарплата во время работы его в отделе de.dept_no
 where de.from_date <= sl.from_date and de.to_date >= sl.to_date
 -- если необходимо  увидеть среднюю зарплату в текущий момент
 -- and sl.to_date > current_date()	
+group by de.dept_no
 -- для проверки
 -- order by emp.emp_no
-);
+) as average_salaries
+inner join lesson3_db.departments as d on d.dept_no = average_salaries.dept_no
+;
+
+-- 2 максимальную зарплату у сотрудника
+select
+	salaries_max.salary_sum
+    , emp.first_name, emp.last_name
+from (
+	select
+		max(sl.salary) as salary_max
+		, emp.emp_no
+	from lesson3_db.salaries as sl
+	inner join lesson3_db.employees as emp on emp.emp_no = sl.emp_no
+    group by emp.emp_no
+) as salaries_max
+inner join lesson3_db.employees as emp on emp.emp_no = salaries_max.emp_no;
 
 select
 *
